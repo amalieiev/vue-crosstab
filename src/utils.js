cb86f5e0cb21d630c7e4d5089a4d803a5593a52c
amalieie <brutal1ty>
2017-08-29 11:33:30 +0300

    added csvJSON converter

--- src/utils.js
+++ src/utils.js
@@ -50,3 +50,23 @@ export const temporalMixin = (datum, item) => {
     datum['day_' + fieldName] = date.toLocaleString('en-us', {weekday: 'long'})
   }
 }
+
+export const csvJSON = function (csv) {
+  let lines = csv.split('\n')
+  let result = []
+
+  let headers = lines[0].split(',')
+
+  for (let i = 1; i < lines.length; i++) {
+    let obj = {}
+    let currentline = lines[i].split(',')
+
+    for (let j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j]
+    }
+
+    result.push(obj)
+  }
+
+  return JSON.parse(JSON.stringify(result))
+}
